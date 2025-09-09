4759f887cdd6edcb24177c998b6cf084716203de
rincon <rincon.diana@uabc.edu.mx>
2025-09-09 03:40:14 -0700

    Agregar una dependencia de ejemplo

---
+++ entidad/src/main/java/mx/desarrollo/entity/NuevaClase.java
@@ -0,0 +1,13 @@
+package mx.desarrollo.entity;
+
+public class NuevaClase {
+    /*Este es un cambio muy importante*/
+
+    public static void main(String[] args){
+        int numero = 10;
+
+        for(int i = 0; i <= numero; i++){
+            System.out.println(i);
+        }
+    }
+}
